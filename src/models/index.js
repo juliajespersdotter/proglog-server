1c2065466e463dd41d82c9331b9318ce2bc01805
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-12-21 14:45:34 +0100

    connection to database established successfully.

--- src/models/index.js
+++ src/models/index.js
@@ -1,18 +1,20 @@
 // Setting up the database connection
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('proglogDB', {
-	host: 'localhost',
+const sequelize = new Sequelize('proglogDB', 'root', null, {
+	host: process.env.DB_HOST,
 	dialect:
 		'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
 })
 
-try {
-	await sequelize.authenticate()
-	console.log('Connection has been established successfully.')
-} catch (error) {
-	console.error('Unable to connect to the database:', error)
-}
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.')
+	})
+	.catch(error => {
+		console.error('Unable to connect to the database: ', error)
+	})
 
 // const models = {}
 // models.Posts = require('./Posts')(bookshelf)
