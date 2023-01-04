37a8a2babcbee48fea7f2093f0ffc8e85c9c4186
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2023-01-04 12:46:44 +0100

    Get one game and all games from RAWG API.

--- src/controllers/api_controller.js
+++ src/controllers/api_controller.js
@@ -1,13 +1,17 @@
-const debug = require('debug')('proglog:authController')
+const debug = require('debug')('proglog:apiController')
 const axios = require('axios').default
 
-const BASE_URL = 'https://api.igdb.com/v4'
-// const client_ID = import.meta.env.VITE_IGDB_CLIENT_ID
-// const access_token = import.meta.env.VITE_IGDB_ACCESS_TOKEN
+// https://api.rawg.io/api/games/3498?key=RAWG_API_TOKEN
+const api_key = process.env.RAWG_API_TOKEN
 
-axios.defaults.baseURL = 'https://api.igdb.com/v4'
-// axios.defaults.headers.post['Client-ID'] = client_ID
-// axios.defaults.headers.post['Authorization'] = `Bearer ${access_token}`
+axios.defaults.baseURL = 'https://api.rawg.io/api'
+// axios.defaults.params['key'] = api_key
+
+const requestOptions = {
+	params: {
+		key: api_key,
+	},
+}
 
 /**
  * Get all games
@@ -15,31 +19,50 @@ axios.defaults.baseURL = 'https://api.igdb.com/v4'
  * GET /
  */
 const getGames = async (req, res) => {
-	axios({
-		url: 'https://api.igdb.com/v4/games',
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Client-ID': 'll5gesoqhzyiaoyiq2n4p1puj2rxqe',
-			Authorization: 'Bearer ffrw3yqzf8qfll44w7bxnnthllc2c9',
-		},
-		data: 'fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;',
-	})
-		.then(response => {
+	try {
+		const result = await axios.get(`/games`, requestOptions)
+
+		if (result) {
+			debug('Accessed data successfully: %0', result.data)
 			res.send({
 				status: 'success',
-				data: response.data,
+				data: result.data,
 			})
+		}
+	} catch (err) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown when attempting to access RAWG API',
 		})
-		.catch(err => {
-			res.status(500).send({
-				status: 'error',
-				message: 'Exception thrown when attempting to access api',
+	}
+}
+
+/**
+ * Get a specific game and info
+ *
+ * GET /:gameId
+ */
+const getGameWithId = async (req, res) => {
+	const gameId = req.params.gameId
+	try {
+		const result = await axios.get(`/games/${gameId}`, requestOptions)
+
+		if (result) {
+			debug('Accessed data successfully: %0', result.data)
+			res.send({
+				status: 'success',
+				data: result.data,
 			})
-			throw err
+		}
+	} catch (err) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown when attempting to access RAWG API',
 		})
+	}
 }
 
 module.exports = {
 	getGames,
+	getGameWithId,
 }
--- src/routes/IGDB_API/api.js
+++ src/routes/IGDB_API/api.js
@@ -5,4 +5,7 @@ const apiController = require('../../controllers/api_controller')
 /** Get all games */
 router.get('/games', apiController.getGames)
 
+/** Get a specific game */
+router.get('/games/:gameId', apiController.getGameWithId)
+
 module.exports = router
